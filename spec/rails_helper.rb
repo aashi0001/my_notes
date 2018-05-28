3cdc5f1495ed06bd20535b002337e8f3cdb451a3
Aasshish Khullar <aasshish.khullar@goomo.com>
2018-05-28 22:20:53 +0530

    boiler plate rails app with rspec configuration with factory girl and full test suite

---
+++ spec/rails_helper.rb
@@ -0,0 +1,43 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require "factory_girl"
+require 'shoulda-matchers'
+require 'simplecov'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+ 
+  SimpleCov.start 'rails'
+ 
+  config.use_transactional_fixtures = true
+  config.include FactoryGirl::Syntax::Methods
+
+  Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+    # Choose one or more libraries:
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
+end
+
+ 
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  
+end
